7599abe94cb09e28e931a89a4f51bda5e770f4ad
Kevin Muhia <kevin.muhia@renga.tech>
2024-08-13 14:43:03 +0300

    Language is set in the nav bar by default

--- assets/js/changeLanguage.js
+++ assets/js/changeLanguage.js
@@ -1,32 +1,42 @@
-    var enButton = document.getElementById('en-btn');
-    var frButton = document.getElementById('fr-btn');
-    var i18n = domI18n({
-      selector: '[data-translatable]',
-      separator: ' // ',
-      languages: ['en', 'fr'],
-      defaultLanguage: 'en'
-    });
+var enButton = document.getElementById('en-btn');
+var frButton = document.getElementById('fr-btn');
+const language = document.getElementById('languageSelector');
+const lang = localStorage.getItem('language');
 
-    enButton.onclick = function () {
-      // i18n.changeLanguage('en');
-      localStorage.setItem('language',  'en')
-      const language = localStorage.getItem('language');
-      i18n.changeLanguage(language)
+if (lang === "fr") {
+   document.getElementById('languageSelector').value = 2
+} else if (lang === "en") {
+   document.getElementById('languageSelector').value = 1
+}
 
-    };
 
-    frButton.onclick = function () {
-      // i18n.changeLanguage('fr');
-      localStorage.setItem('language',  'fr')
-      const language = localStorage.getItem('language');
-      i18n.changeLanguage(language)
+var i18n = domI18n({
+  selector: '[data-translatable]',
+  separator: ' // ',
+  languages: ['en', 'fr'],
+  defaultLanguage: 'en'
+});
 
-    }
-    const pageLoad = () => {
-      //fetch language on local storage
-      const language = localStorage.getItem('language')
-      i18n.changeLanguage(language)
-      console.log("Page Loading")
-    }
+enButton.onclick = function () {
+  // i18n.changeLanguage('en');
+  localStorage.setItem('language',  'en')
+  const language = localStorage.getItem('language');
+  i18n.changeLanguage(language)
+
+};
+
+frButton.onclick = function () {
+  // i18n.changeLanguage('fr');
+  localStorage.setItem('language',  'fr')
+  const language = localStorage.getItem('language');
+  i18n.changeLanguage(language)
+
+}
+const pageLoad = () => {
+  //fetch language on local storage
+  const language = localStorage.getItem('language')
+  i18n.changeLanguage(language)
+  console.log("Page Loading")
+}
 
 pageLoad()
